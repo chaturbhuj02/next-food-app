d6f591e6ebb459b60a267064a81b8857f405962f
Chatarbhuj Bhattarai <chaturbhuj02@gmail.com>
2024-05-02 19:44:15 +1000

    adding client components

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,29 +1,30 @@
 import Link from "next/link";
-import logoImg from '@/assets/logo.png';
-import classes from './main-header.module.css'
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
-export default function MainHeader(){
-    return (
-        <>
-        <MainHeaderBackground />
-        <header>
-            <Link href='/' className={classes.logo}>
-                <Image src={logoImg} alt="a plate with food on it" priority />
-                Next Level Food
-            </Link>
-
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href='/meals' >Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href='/community' >Foodies Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header></>
-    )
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logoImg} alt="a plate with food on it" priority />
+          Next Level Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
